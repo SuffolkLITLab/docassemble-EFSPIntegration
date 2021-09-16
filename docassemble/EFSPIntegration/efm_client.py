a9994a8fc946d6e341e70bd179f27491be762551
Quinten Steenhuis <qsteenhuis@gmail.com>
2021-09-16 18:59:09 -0400

    Picklist for payment account ID

--- docassemble/EFSPIntegration/efm_client.py
+++ docassemble/EFSPIntegration/efm_client.py
@@ -2,13 +2,13 @@
 import re
 import json
 import logging
-from typing import List
+from typing import Union,List, Dict
 import http.client as http_client
 
 from urllib.parse import urlencode
 import requests
 from docassemble.base.functions import all_variables, get_config
-from docassemble.base.util import IndividualName, DAObject, log, Person
+from docassemble.base.util import IndividualName, DAObject, log, Person, Individual
 from docassemble.AssemblyLine.al_document import ALDocumentBundle
 
 __all__ = ['ApiResponse','ProxyConnection']
@@ -376,9 +376,12 @@ class ProxyConnection:
         data=json.dumps(kwargs))
     return self._call_proxy(send)
 
-  def create_service_contact(self, service_contact):
+  def create_service_contact(self, service_contact:Individual):
+    service_contact_dict = {
+
+    }
     send = lambda: self.proxy_client.post(self.base_url + f'firmattorneyservice/service-contacts', 
-        data=json.dumps(service_contact))
+        data=json.dumps(service_contact_dict))
     return self._call_proxy(send)
 
   def remove_service_contact(self, service_contact_id):
@@ -517,4 +520,34 @@ class ProxyConnection:
   def get_disclaimers(self, court_id:str):
     send = lambda: self.proxy_client.get(self.base_url + f'codes/courts/{court_id}/disclaimer_requirements')
     return self._call_proxy(send)
+  
+
+def serialize_person(person:Union[Person,Individual])->Dict:
+  """
+  Converts a Docassemble Person or Individual into a dictionary suitable for
+  json.dumps and in format expected by the EFSPProxy
+  """
+  if isinstance(person, Individual):
+    return_dict = {
+      "firstName": person.name.first,
+      "middleName": person.name.middle if hasattr(person.name, 'middle') else None,
+      "lastName": person.name.last,
+    }
+  else:
+    return_dict = {
+      "firmName": person.name.text,
+    }
+  return_dict.update({
+    "address": {
+      "addressLine1": person.address.address,
+      "addressLine2": person.address.unit if hasattr(person.address, 'unit') else None,
+      "city": person.address.city,
+      "state": person.address.state,
+      "zipCode": person.address.zip,
+      "country": person.address.country if hasattr(person.address, 'country') else None,
+    },
+    "phoneNumber": person.phone_number if hasattr(person, 'phone_number') else None,
+  })
+
+  return return_dict
   
