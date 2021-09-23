bd549db7b1e70bea96bec95434afb19d11109d02
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-09-23 00:18:38 -0400

    Better display of Nested XML->Java Obj -> JSON -> python dict data

--- docassemble/EFSPIntegration/conversions.py
+++ docassemble/EFSPIntegration/conversions.py
@@ -14,3 +14,20 @@ def choices_and_map(codes_list:List, display:str=None, backing:str=None) -> Tupl
   choices_list = list(map(lambda vv: (vv[backing], display.format(**vv)), codes_list))
   codes_map = { vv[backing] : vv for vv in codes_list }
   return choices_list, codes_map
+
+def pretty_display(data, tab_depth=0):
+  tab_inc = 4
+  out = ''
+  if isinstance(data, list):
+    for idx, elem in enumerate(data):
+      out += (' ' * tab_depth) + f'* idx: {idx}\n'
+      out += pretty_display(elem, tab_depth + tab_inc)
+
+  elif isinstance(data, dict):
+    for key, val in data.items():
+      if val is not None and val != []:
+        out += (' ' * tab_depth) + f'* {key}: \n'
+        out += pretty_display(val, tab_depth + tab_inc)
+  else:
+    out = (' ' * tab_depth) + str(data) + '\n'
+  return out
