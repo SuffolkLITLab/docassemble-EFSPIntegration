f6692bde0cf0ffd1890d662621b474e3fd53d1e8
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2021-12-15 12:35:19 -0500

    Make found_case.participants DAObjects with proper names

--- docassemble/EFSPIntegration/test/test_conversions.py
+++ docassemble/EFSPIntegration/test/test_conversions.py
@@ -7,7 +7,7 @@ from docassemble.AssemblyLine.al_general import ALIndividual
 from ..efm_client import ProxyConnection, ApiResponse
 from ..conversions import parse_case_info
 
-class test_coversions(unittest.TestCase):
+class TestConversions(unittest.TestCase):
   def setUp(self):
     with open(Path(__file__).parent / 'vars.json', 'r') as file:
       full_json = json.load(file).get('variables')
@@ -17,8 +17,9 @@ class test_coversions(unittest.TestCase):
 
   def test_parse_case_info(self):
     """Makes sure that participants of the case are parsed fully, needed """
-    case = DAObject()
+    case = DAObject('case')
     parse_case_info(self.proxy_conn, case, self.my_var, 'adams', {})
+    self.assertEqual(len(case.participants), 2)
     for partip in case.participants:
       self.assertIsInstance(partip, ALIndividual)
       self.assertIn(partip.person_type, ['ALIndividual', 'business'])
@@ -27,3 +28,4 @@ class test_coversions(unittest.TestCase):
         self.assertNotEqual(partip.name.last, None)
       self.assertTrue(hasattr(partip, 'role_code'))
       self.assertTrue(hasattr(partip, 'role_name'))
+    self.assertEqual(case.participants[0].instanceName, 'case.participants[0]')
