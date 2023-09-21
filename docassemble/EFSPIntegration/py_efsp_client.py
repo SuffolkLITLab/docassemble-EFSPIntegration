55207d205d6d1adb2e80957f46eda9f58a36ca66
Bryce Willey <Bryce.Steven.Willey@gmail.com>
2023-09-21 14:31:16 -0400

    Python filing

--- docassemble/EFSPIntegration/py_efsp_client.py
+++ docassemble/EFSPIntegration/py_efsp_client.py
@@ -878,7 +878,9 @@ class EfspConnection:
         return self._call_proxy(send)
 
     def get_filer_types(self, court_id: str) -> ApiResponse:
-        send = lambda: self.proxy_client.get(self.full_url(f"codes/courts/{court_id}/filer_types"))
+        send = lambda: self.proxy_client.get(
+            self.full_url(f"codes/courts/{court_id}/filer_types")
+        )
         return self._call_proxy(send)
 
     def get_case_types(
